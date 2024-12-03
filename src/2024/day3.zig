89acbc17956b47142dc25e4071981f9a7c0c6639
TsunamiNoAi <spam@falseblue.com>
2024-12-03 11:51:57 -0500

    part 1

--- src/2024/day3.zig
+++ src/2024/day3.zig
@@ -3,17 +3,28 @@ const mvzr = @import("mvzr");
 
 pub const DayNumber = 3;
 
-pub const Answer1 = 0;
+pub const Answer1 = 174336360;
 pub const Answer2 = 0;
 
 pub fn part1(in: []const u8) f32 {
     const regex = mvzr.Regex.compile("mul\\(([0-9]+),([0-9]+)\\)") orelse unreachable;
-    const ret: f32 = 0;
-    var iter = regex.iterator(in);
-    while (iter.next()) |match| {
-        std.debug.print("{any}\n", .{match});
+    var ret: i32 = 0;
+    var line_iter = std.mem.splitScalar(u8, in, '\n');
+    while (line_iter.next()) |line| {
+        var iter = regex.iterator(line);
+        while (iter.next()) |match| {
+            const numbers = match.slice[4 .. match.slice.len - 1];
+            // std.debug.print("numbers:{s}\n", .{numbers});
+
+            const split_idx = std.mem.indexOf(u8, numbers, ",") orelse unreachable;
+            const num1 = std.fmt.parseInt(i32, numbers[0..split_idx], 10) catch unreachable;
+            const num2 = std.fmt.parseInt(i32, numbers[split_idx + 1 ..], 10) catch unreachable;
+            const mul = num1 * num2;
+            ret += mul;
+            // std.debug.print("{} x {} = {}: {}\n", .{ num1, num2, mul, ret });
+        }
     }
-    return ret;
+    return @floatFromInt(ret);
 }
 pub fn part2(in: []const u8) f32 {
     const ret: f32 = 0;
