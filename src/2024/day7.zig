4c791ab9819c8ab8e21857b815b8b99f765ebf0a
TsunamiNoAi <spam@falseblue.com>
2024-12-07 17:35:49 -0500

    Part 1 complete

--- src/2024/day7.zig
+++ src/2024/day7.zig
@@ -4,21 +4,21 @@ const tst = std.testing;
 
 pub const DayNumber = 7;
 
-pub const Answer1 = 0;
+pub const Answer1 = 12553187650171;
 pub const Answer2 = 0;
 
 const Equation = struct {
-    result: i32,
-    operands: Array(i32),
+    result: i64,
+    operands: Array(i64),
     valid_mask: ?usize = null,
 
     pub fn init(alloc: std.mem.Allocator, in: []const u8) !Equation {
         const colon = std.mem.indexOfScalar(u8, in, ':') orelse return error.InvalidEq;
-        const res = try std.fmt.parseInt(i32, in[0..colon], 10);
-        var list = Array(i32).init(alloc);
+        const res = try std.fmt.parseInt(i64, in[0..colon], 10);
+        var list = Array(i64).init(alloc);
         var iter = std.mem.splitScalar(u8, in[colon + 2 ..], ' ');
         while (iter.next()) |num| {
-            try list.append(try std.fmt.parseInt(i32, num, 10));
+            try list.append(try std.fmt.parseInt(i64, num, 10));
         }
 
         return .{
@@ -34,37 +34,39 @@ const Equation = struct {
         try writer.print("{} = ", .{self.result});
         for (self.operands.items, 0..) |num, idx| {
             const select = @as(usize, @intCast(1)) << @intCast(idx);
-            try writer.print("{} ", .{num});
+            try writer.print("{}", .{num});
             if (self.valid_mask) |mask| {
-                if (idx < self.operands.items.len - 1)
-                    try writer.print("{s} ", .{if (mask & (select) == select) "*" else "+"});
-            }
+                if (idx < self.operands.items.len - 1) {
+                    try writer.print(" {s} ", .{if (mask & (select) == select) "*" else "+"});
+                }
+            } else if (idx < self.operands.items.len - 1) try writer.writeAll(" ? ");
         }
     }
-    const Operator = *const fn (i32, i32) i32;
-    pub fn eval(self: Equation, op: Operator) i32 {
+    const Operator = *const fn (i64, i64) i64;
+    pub fn eval(self: Equation, op: Operator) i64 {
         return op(self.operands.items[0], self.operands.items[1]);
     }
     pub fn can_be_evaluated(self: *Equation) !bool {
         var opns = try self.operands.clone();
         defer opns.deinit();
 
-        var acc: i32 = 0;
+        var acc: i64 = 0;
         const num_operands = opns.items.len;
-        const num_operations: usize = (num_operands - 1) * 2;
-        for (0..num_operations) |op_mask| {
+        const operations: usize = @as(usize, @intCast(1)) << @intCast(num_operands - 1);
+        // std.debug.print("Evaluating: {} with {} {b:0>4} ops\n", .{ self, operations, operations });
+        for (0..operations) |op_vec| {
             acc = opns.items[0];
-            for (0..num_operands - 1) |operand_idx| {
+            // std.debug.print("Evaluating: {} with vector {b:0>4}\n", .{ self, op_vec });
+            for (0..num_operands - 1) |op_idx| {
                 const left = acc;
-                const right = opns.items[operand_idx + 1];
+                const right = opns.items[op_idx + 1];
                 // std.debug.print("{} ", .{left});
                 const op = blk: {
-                    // std.debug.print("M:b{b:3} ", .{op_mask});
-                    // std.debug.print("O:b{b:3} ", .{operand_idx - 1});
-                    const select = @as(usize, @intCast(1)) << @intCast(operand_idx + 1 - 1);
-                    // std.debug.print("S:b{b:3} ", .{select});
+                    // std.debug.print("O:b{b:0>4} ", .{op_idx});
+                    const select = @as(usize, @intCast(1)) << @intCast(op_idx);
+                    // std.debug.print("S:b{b:0>4} ", .{select});
 
-                    if (op_mask & (select) == select) {
+                    if (op_vec & (select) == select) {
                         // std.debug.print("* ", .{});
                         break :blk &Mul;
                     } else {
@@ -81,28 +83,29 @@ const Equation = struct {
 
             // std.debug.print("{} ?= {}\n", .{ acc, self.result });
             if (acc == self.result) {
-                self.valid_mask = op_mask;
-                std.debug.print("{} Evaluates\n", .{self});
+                self.valid_mask = op_vec;
+                // std.debug.print("{} Evaluates\n", .{self});
                 return true;
             }
         }
-        std.debug.print("{} Bailed out\n", .{self});
+        // std.debug.print("{} Bailed out\n", .{self});
         return false;
     }
 };
 test {
     var eq = try Equation.init(tst.allocator, "292: 11 6 16 20");
     defer eq.deinit();
-    try tst.expect(try eq.can_be_evaluated());
-
-    var eq2 = try Equation.init(tst.allocator, "190: 10 19");
+    // try tst.expect(try eq.can_be_evaluated());
+}
+test {
+    var eq2 = try Equation.init(tst.allocator, "44: 2 2 2 2 2 2 2");
     defer eq2.deinit();
     try tst.expect(try eq2.can_be_evaluated());
 }
-fn Mul(left: i32, right: i32) i32 {
+fn Mul(left: i64, right: i64) i64 {
     return left * right;
 }
-fn Add(left: i32, right: i32) i32 {
+fn Add(left: i64, right: i64) i64 {
     return left + right;
 }
 
@@ -111,7 +114,7 @@ fn Add(left: i32, right: i32) i32 {
 ///TODO: Evalute if equation is solvable
 ///TODO: Output sum of solveable equations
 pub fn part1(in: []const u8) f32 {
-    var ret: i32 = 0;
+    var ret: i64 = 0;
     var iter = std.mem.splitScalar(u8, in, '\n');
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
@@ -120,14 +123,16 @@ pub fn part1(in: []const u8) f32 {
     defer eqs.deinit();
 
     while (iter.next()) |line| {
+        if (line.len == 0) continue;
         var eq = Equation.init(alloc, line) catch unreachable;
         defer eq.deinit();
+        // std.debug.print("Loading: {s}\n", .{line});
         if ((eq.can_be_evaluated() catch unreachable)) {
-            std.debug.print("Eq {any} can be evaluated\n", .{eq});
             ret += eq.result;
+            // std.debug.print("{} += {}\n", .{ ret - eq.result, eq });
         }
     }
-    std.debug.print("{}\n", .{ret});
+    // std.debug.print("{}\n", .{ret});
     return @floatFromInt(ret);
 }
 pub fn part2(in: []const u8) f32 {
