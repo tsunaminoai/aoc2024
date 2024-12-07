cf481674b24659755cf21fc19dafa7ceab3cd124
TsunamiNoAi <spam@falseblue.com>
2024-12-07 13:54:08 -0500

    start day 7

--- build.zig
+++ build.zig
@@ -20,11 +20,7 @@ pub fn build(b: *std.Build) !void {
     b.installArtifact(lib);
     const run_all_step = b.step("run", "Run all days");
 
-<<<<<<< HEAD
-    for (1..5) |n| {
-=======
-    for (1..6) |n| {
->>>>>>> origin/day5
+    for (1..7) |n| {
         const exe = b.addExecutable(.{
             .name = b.fmt("aoc_{}", .{n}),
             .root_source_file = b.path("src/main.zig"),
