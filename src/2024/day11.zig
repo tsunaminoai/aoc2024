e78a09a1f48c8cb8c3faffafd5639a98ff3c5484
TsunamiNoAi <spam@falseblue.com>
2024-12-11 11:28:33 -0500

    add precalcs

--- src/2024/day11.zig
+++ src/2024/day11.zig
@@ -46,34 +46,29 @@ pub fn part2(in: []const u8) f32 {
     }
     return @floatFromInt(stones.items.len);
 }
+pub fn blink(stones: *std.ArrayList(i64)) !void {
+    // Preallocate memory to avoid frequent reallocations
+    try stones.ensureTotalCapacity(stones.items.len * 2);
 
-pub fn blink(stones: *Array(i64)) !void {
     var idx: usize = 0;
     while (idx < stones.items.len) : (idx += 1) {
         const stone = &stones.items[idx];
-        const num_digits = std.math.floor(@log10(@as(f32, @floatFromInt(stone.*)))) + 1;
         if (stone.* == 0) {
             stone.* = 1;
-        } else if (@mod(num_digits, 2) == 0) {
-            const split: i64 = std.math.pow(i64, 10, @as(i64, @intFromFloat((num_digits / 2))));
-            // std.debug.print(
-            //     "even digits {} {} {}\n",
-            //     .{
-            //         stone.*,
-            //         @divFloor(stone.*, split),
-            //         @mod(stone.*, split),
-            //     },
-            // );
-            const val = stone.*;
-
-            try stones.insert(idx + 1, @mod(val, split));
-            stones.items[idx] = @divFloor(val, split);
-            idx += 1;
         } else {
-            stone.* *= 2024;
+            const num_digits = comptime_int_log10(stone.*);
+            if (num_digits % 2 == 0) {
+                // std.debug.print("{}\n", .{num_digits});
+                const split = comptime_pow10(num_digits / 2);
+                const val = stone.*;
+                try stones.insert(idx + 1, @mod(val, split));
+                stones.items[idx] = @divFloor(val, split);
+                idx += 1;
+            } else {
+                stone.* *= 2024;
+            }
         }
     }
-    // std.debug.print("{any}\n", .{stones.items});
 }
 
 const test_input =
@@ -83,4 +78,50 @@ const test_input =
 test {
     try std.testing.expectEqual(55312, part1(test_input));
     try std.testing.expectEqual(0, part2(test_input));
+}
+
+inline fn comptime_int_log10(x: i64) usize {
+    return switch (x) {
+        0...9 => 1,
+        10...99 => 2,
+        100...999 => 3,
+        1000...9999 => 4,
+        10000...99999 => 5,
+        100000...999999 => 6,
+        1000000...9999999 => 7,
+        10000000...99999999 => 8,
+        100000000...999999999 => 9,
+        1000000000...9999999999 => 10,
+        10000000000...99999999999 => 11,
+        100000000000...999999999999 => 12,
+        1000000000000...9999999999999 => 13,
+        10000000000000...99999999999999 => 14,
+        100000000000000...999999999999999 => 15,
+        1000000000000000...9999999999999999 => 16,
+        else => 19,
+    };
+}
+
+inline fn comptime_pow10(n: usize) i64 {
+    // std.debug.print("{}\n", .{n});
+    return switch (n) {
+        0 => 1,
+        1 => 10,
+        2 => 100,
+        3 => 1000,
+        4 => 10000,
+        5 => 100000,
+        6 => 1000000,
+        7 => 10000000,
+        8 => 100000000,
+        9 => 1000000000,
+        10 => 10000000000,
+        11 => 100000000000,
+        12 => 1000000000000,
+        13 => 10000000000000,
+        14 => 100000000000000,
+        15 => 1000000000000000,
+        16 => 10000000000000000,
+        else => 0,
+    };
 }
