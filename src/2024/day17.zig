c53583b0317e5d5d9b8d1f2e9494c88085f23015
TsunamiNoAi <spam@falseblue.com>
2024-12-17 21:24:52 -0500

    get threads running

--- src/2024/day17.zig
+++ src/2024/day17.zig
@@ -5,7 +5,7 @@ const tst = std.testing;
 const math = std.math;
 const lib = @import("lib");
 
-pub const DayNumber = 0;
+pub const DayNumber = 17;
 
 pub const Answer1 = 0;
 pub const Answer2 = 0;
@@ -13,12 +13,96 @@ pub const Answer2 = 0;
 pub fn part1(in: []const u8) f32 {
     const ret: f32 = 0;
     _ = in;
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const alloc = gpa.allocator();
+    var p = Processor.init(alloc, 27575648, 0, 0);
+    defer p.deinit();
+
+    p.load("2,4,1,2,7,5,4,1,1,3,5,5,0,3,3,0") catch unreachable;
+    p.run() catch unreachable;
+
+    var str = Array(u8).init(alloc);
+    defer str.deinit();
+    for (p.outputs.items, 0..) |o, i| {
+        str.append(@intCast(o + 48)) catch unreachable;
+        if (i < p.outputs.items.len - 1) {
+            str.append(',') catch unreachable;
+        }
+    }
+    std.debug.print("'{s}'\n", .{str.items});
+
     return ret;
 }
+
+fn worker(start: usize, len: usize, results: *Array(i64)) void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const alloc = gpa.allocator();
+
+    for (start..start + len) |inputA| {
+        var p: *Processor = alloc.create(Processor) catch unreachable;
+        defer alloc.destroy(p);
+        if (inputA % 100_000 == 0) std.debug.print("{}\n", .{inputA});
+        p.* = Processor.init(alloc, inputA, 0, 0);
+        defer p.deinit();
+
+        p.load("2,4,1,2,7,5,4,1,1,3,5,5,0,3,3,0") catch unreachable;
+        p.run() catch {};
+
+        if (std.mem.eql(u64, p.outputs.items, &.{ 2, 4, 1, 2, 7, 5, 4, 1, 1, 3, 5, 5, 0, 3, 3, 0 })) {
+            results.append(@intCast(inputA)) catch unreachable;
+        }
+    }
+}
+
 pub fn part2(in: []const u8) f32 {
-    const ret: f32 = 0;
+    var ret: f32 = 0;
     _ = in;
-    return ret;
+
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const alloc = gpa.allocator();
+
+    const num_threads = std.Thread.getCpuCount() catch unreachable;
+    const batch_size = 100_000;
+
+    var pool: std.Thread.Pool = undefined;
+    pool.init(.{
+        .allocator = alloc,
+        .n_jobs = @intCast(num_threads),
+    }) catch unreachable;
+    defer pool.deinit();
+    var wait_group: std.Thread.WaitGroup = undefined;
+    wait_group.reset();
+
+    var results = Array(i64).init(alloc);
+    defer results.deinit();
+
+    var idx: usize = 40_900_000;
+    while (idx < 100_000_000) {
+        wait_group.reset();
+        for (num_threads) |_| {
+            pool.spawn(worker, .{
+                idx,
+                batch_size,
+                &results,
+            }) catch unreachable;
+            idx += batch_size;
+        }
+        pool.waitAndWork(&wait_group);
+        if (results.getLastOrNull()) |_| {
+            std.mem.sort(i64, results.items, .{}, isLessThan);
+            ret = @floatFromInt(results.items[0]);
+            break;
+        }
+    }
+
+    std.debug.print("\n\n{}\n\n", .{ret});
+    return 0;
+}
+fn isLessThan(_: @TypeOf(.{}), a: i64, b: i64) bool {
+    return a < b;
 }
 const test_input =
     \\
@@ -30,22 +114,24 @@ test {
 }
 
 const Processor = struct {
-    A: i64 = 0,
-    B: i64 = 0,
-    C: i64 = 0,
+    A: u64 = 0,
+    B: u64 = 0,
+    C: u64 = 0,
     tape: Array(Intruction),
-    outputs: Array(i64),
+    outputs: Array(u64),
 
-    var intruction_pointer: usize = 0;
-    var isRunning: bool = true;
+    intruction_pointer: usize = 0,
+    isRunning: bool = true,
 
-    pub fn init(alloc: Allocator, a: i64, b: i64, c: i64) Processor {
+    pub fn init(alloc: Allocator, a: u64, b: u64, c: u64) Processor {
         return .{
             .A = a,
             .B = b,
             .C = c,
             .tape = Array(Intruction).init(alloc),
-            .outputs = Array(i64).init(alloc),
+            .outputs = Array(u64).init(alloc),
+            .intruction_pointer = 0,
+            .isRunning = true,
         };
     }
     pub fn load(self: *Processor, in: []const u8) !void {
@@ -55,16 +141,17 @@ const Processor = struct {
         }
     }
     pub fn run(self: *Processor) !void {
-        while (isRunning) {
+        while (self.isRunning) {
             try self.tick();
-            std.debug.print("Result: {}\n", .{self});
+            // std.debug.print("Result: {}\n", .{self});
         }
     }
 
-    fn get_operand(self: Processor, comptime T: type) !T {
-        const op_idx = intruction_pointer + 1;
+    fn get_operand(self: Processor, comptime T: type, isLiteral: bool) !T {
+        const op_idx = self.intruction_pointer + 1;
         if (op_idx >= self.tape.items.len) return error.OperandBeyondTape;
         const num = self.tape.items[op_idx].toInt(u3);
+        if (isLiteral) return num;
         return switch (num) {
             0...3 => @intCast(num),
             4 => @intCast(self.A),
@@ -74,35 +161,37 @@ const Processor = struct {
         };
     }
     pub fn tick(self: *Processor) !void {
-        if (intruction_pointer >= self.tape.items.len) {
-            std.debug.print("Halt\n", .{});
-            isRunning = false;
+        if (self.intruction_pointer >= self.tape.items.len - 1) {
+            // std.debug.print("Halt\n", .{});
+            self.isRunning = false;
             return;
         }
-        const next_op = self.tape.items[intruction_pointer];
-        defer intruction_pointer += 2;
+        const next_op = self.tape.items[self.intruction_pointer];
 
-        std.debug.print("Processing instruction: {s}\n", .{@tagName(next_op)});
+        // std.debug.print("Processing instruction: {s}\n", .{@tagName(next_op)});
         switch (next_op) {
             // The adv instruction (opcode 0) performs division. The numerator is the value in the A register.
             //  The denominator is found by raising 2 to the power of the instruction's combo operand. (So, an operand
             //  of 2 would divide A by 4 (2^2); an operand of 5 would divide A by 2^B.) The result of the division operation
             //  is truncated to an integer and then written to the A register.
             .adv => {
-                const num: i64 = self.A;
-                const denom: i64 = @as(i64, 1) << try self.get_operand(u6);
+                const num: u64 = self.A;
+                const denom: u64 = @as(u64, 1) << @truncate(@as(u64, @intCast(try self.get_operand(i64, false))));
                 self.A = @divTrunc(num, denom);
             },
             // The bxl instruction (opcode 1) calculates the bitwise XOR of register B and the instruction's literal operand,
             //   then stores the result in register B.
             .bxl => {
-                self.B = self.B ^ try self.get_operand(i64);
+                self.B = self.B ^ try self.get_operand(u64, true);
             },
 
             // The bst instruction (opcode 2) calculates the value of its combo operand modulo 8 (thereby keeping only its
             //   lowest 3 bits), then writes that value to the B register.
             .bst => {
-                self.B = @mod(try self.get_operand(i64), 8);
+                // std.debug.print("\t{}\n", .{
+                //     try self.get_operand(u64, false),
+                // });
+                self.B = @mod(try self.get_operand(u64, false), 8);
             },
 
             // The jnz instruction (opcode 3) does nothing if the A register is 0. However, if the A register is not zero,
@@ -110,7 +199,7 @@ const Processor = struct {
             //  the instruction pointer is not increased by 2 after this instruction.
             .jnz => {
                 if (self.A != 0) {
-                    intruction_pointer = try self.get_operand(usize);
+                    self.intruction_pointer = try self.get_operand(usize, true);
                     return;
                 }
             },
@@ -124,25 +213,26 @@ const Processor = struct {
             // The out instruction (opcode 5) calculates the value of its combo operand modulo 8, then outputs that value.
             //   (If a program outputs multiple values, they are separated by commas.)
             .out => {
-                try self.outputs.append(@mod(try self.get_operand(i64), 8));
+                try self.outputs.append(@mod(try self.get_operand(u64, false), 8));
             },
 
             // The bdv instruction (opcode 6) works exactly like the adv instruction except that the result is stored in the
             //   B register. (The numerator is still read from the A register.)
             .bdv => {
-                const num: i64 = self.A;
-                const denom: i64 = @as(i64, 1) << try self.get_operand(u6);
+                const num: u64 = self.A;
+                const denom: u64 = @as(u64, 1) << try self.get_operand(u6, false);
                 self.B = @divTrunc(num, denom);
             },
 
             // The cdv instruction (opcode 7) works exactly like the adv instruction except that the result is stored in the
             //   C register. (The numerator is still read from the A register.)
             .cdv => {
-                const num: i64 = self.A;
-                const denom: i64 = @as(i64, 1) << try self.get_operand(u6);
+                const num: u64 = self.A;
+                const denom: u64 = @as(u64, 1) << @truncate(@as(u64, @intCast(try self.get_operand(i64, false))));
                 self.C = @divTrunc(num, denom);
             },
         }
+        self.intruction_pointer += 2;
     }
 
     pub fn deinit(self: *Processor) void {
@@ -157,8 +247,8 @@ const Processor = struct {
             self.A,
             self.B,
             self.C,
-            intruction_pointer,
-            if (intruction_pointer < self.tape.items.len) @tagName(self.tape.items[intruction_pointer]) else "N/A",
+            self.intruction_pointer,
+            if (self.intruction_pointer < self.tape.items.len) @tagName(self.tape.items[self.intruction_pointer]) else "N/A",
         });
     }
 };
@@ -179,23 +269,68 @@ const Intruction = enum(u3) {
     }
 };
 
-test {
+test "1" {
     var p1 = Processor.init(tst.allocator, 0, 0, 9);
     defer p1.deinit();
     try p1.load("2,6");
     try p1.run();
     try tst.expectEqual(1, p1.B);
-
+}
+test "2" {
     var p2 = Processor.init(tst.allocator, 10, 0, 0);
     defer p2.deinit();
     try p2.load("5,0,5,1,5,4");
+
     try p2.run();
-    try tst.expectEqualSlices(i64, &.{ 0, 1, 2 }, p2.outputs.items);
-    // var p = Processor.init(tst.allocator, 729, 0, 0);
-    // defer p.deinit();
-    // p.C = 9;
+    try tst.expectEqual(6, p2.tape.items.len);
+    try tst.expectEqualSlices(u64, &.{ 0, 1, 2 }, p2.outputs.items);
+}
 
-    // try p.load("0,1,5,4,3,0");
+test "3" {
+    var p = Processor.init(tst.allocator, 2024, 0, 0);
+    defer p.deinit();
+    try p.load("0,1,5,4,3,0");
 
-    // try tst.expectEqual(6, p.tape.items.len);
+    try p.run();
+
+    try tst.expectEqual(0, p.A);
+    try tst.expectEqualSlices(
+        u64,
+        &.{ 4, 2, 5, 6, 7, 7, 7, 7, 3, 1, 0 },
+        p.outputs.items,
+    );
+}
+
+test "4" {
+    var p = Processor.init(tst.allocator, 0, 29, 0);
+    defer p.deinit();
+    try p.load("1,7");
+
+    try p.run();
+
+    try tst.expectEqual(26, p.B);
+}
+
+test "5" {
+    var p = Processor.init(tst.allocator, 0, 2024, 43690);
+    defer p.deinit();
+    try p.load("4,0");
+
+    try p.run();
+
+    try tst.expectEqual(44354, p.B);
+}
+
+test "larger" {
+    var p = Processor.init(tst.allocator, 729, 0, 0);
+    defer p.deinit();
+    try p.load("0,1,5,4,3,0");
+
+    try p.run();
+
+    try tst.expectEqualSlices(
+        u64,
+        &.{ 4, 6, 3, 5, 6, 3, 5, 2, 1, 0 },
+        p.outputs.items,
+    );
 }
