f9c3cb3a5cf5fa24c3a1b36dcf47783809bdc719
Tsunami Noai <spam@falseblue.com>
2025-12-05 11:29:09 -0500

    Add failing day 5

--- src/2025/day05.zig
+++ src/2025/day05.zig
@@ -7,9 +7,12 @@ const tst = std.testing;
 const math = std.math;
 
 // Automatically embedded at compile time
-pub const data = @embedFile("data/day01.txt");
-pub const DayNumber = 1;
+pub const data = @embedFile("data/day05.txt");
+pub const DayNumber = 5;
 
+/// The fresh ID ranges are inclusive: the range 3-5 means that ingredient IDs 3, 4, and 5 are all fresh.
+/// The ranges can also overlap; an ingredient ID is fresh if it is in any range.
+/// The Elves are trying to determine which of the available ingredient IDs are fresh.
 pub fn part1(allocator: std.mem.Allocator, input: []const u8) !i64 {
     _ = allocator; // autofix
     const result: i64 = 0;
@@ -35,16 +38,27 @@ pub fn part2(allocator: std.mem.Allocator, input: []const u8) !i64 {
 
     return result;
 }
-
+/// The database operates on ingredient IDs. It consists of a list of fresh ingredient ID ranges,
+/// a blank line, and a list of available ingredient IDs. For example:
 const test_input =
-    \\example input
+    \\3-5
+    \\10-14
+    \\16-20
+    \\12-18
+    \\
+    \\1
+    \\5
+    \\8
+    \\11
+    \\17
+    \\32
 ;
 
 test "part 1" {
     const example = test_input;
 
     const result = try part1(std.testing.allocator, example);
-    try std.testing.expectEqual(@as(i64, 0), result);
+    try std.testing.expectEqual(@as(i64, 3), result);
 }
 
 test "part 2" {
