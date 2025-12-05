d9df15ed6eec922ef660df7809a24e52a9466230
Tsunami Noai <spam@falseblue.com>
2025-12-05 11:59:24 -0500

    Tests pass, but also im silly

--- src/2025/day05.zig
+++ src/2025/day05.zig
@@ -34,16 +34,14 @@ pub fn part1(allocator: std.mem.Allocator, input: []const u8) !i64 {
 }
 
 pub fn part2(allocator: std.mem.Allocator, input: []const u8) !i64 {
-    _ = allocator; // autofix
-    const result: i64 = 0;
+    var sections = std.mem.splitSequence(u8, input, "\n\n");
+    const db_section = sections.next() orelse return error.InvalidInput;
+    var db = Database.init(allocator);
+    defer db.deinit();
 
-    var lines = std.mem.tokenizeScalar(u8, input, '\n');
-    while (lines.next()) |line| {
-        _ = line; // autofix
-        // Your solution here
-    }
+    try db.readDBFromString(db_section);
 
-    return result;
+    return db.ids.count();
 }
 
 const Range = struct {
@@ -53,19 +51,32 @@ const Range = struct {
     pub fn contains(self: Range, value: usize) bool {
         return self.start <= value and value <= self.stop;
     }
+    pub fn validIds(self: Range, alloc: Allocator) ![]usize {
+        var ret = Array(usize){};
+        defer ret.deinit(alloc);
+
+        for (self.start..self.stop + 1) |id| {
+            try ret.append(alloc, id);
+        }
+
+        return try ret.toOwnedSlice(alloc);
+    }
 };
 const Database = struct {
     ranges: Array(Range) = .{},
     alloc: Allocator,
+    ids: std.AutoHashMap(usize, usize),
 
     pub fn init(alloc: Allocator) Database {
         return .{
             .ranges = .{},
             .alloc = alloc,
+            .ids = std.AutoHashMap(usize, usize).init(alloc),
         };
     }
     pub fn deinit(self: *Database) void {
         self.ranges.deinit(self.alloc);
+        self.ids.deinit();
     }
     pub fn readDBFromString(self: *Database, str: []const u8) !void {
         var lines = std.mem.tokenizeScalar(u8, str, '\n');
@@ -78,7 +89,15 @@ const Database = struct {
     }
 
     pub fn addRange(self: *Database, start: usize, stop: usize) !void {
-        try self.ranges.append(self.alloc, .{ .start = start, .stop = stop });
+        const newRange = Range{ .start = start, .stop = stop };
+        const newIds = try newRange.validIds(self.alloc);
+        defer self.alloc.free(newIds);
+
+        for (newIds) |id| {
+            try self.ids.put(id, id);
+        }
+
+        try self.ranges.append(self.alloc, newRange);
     }
     pub fn isFresh(self: Database, id: usize) bool {
         for (self.ranges.items) |r| {
@@ -103,6 +122,12 @@ const test_input =
     \\32
 ;
 
+test "range" {
+    const ret = try (Range{ .start = 3, .stop = 5 }).validIds(tst.allocator);
+    defer tst.allocator.free(ret);
+    try tst.expectEqual(3, ret.len);
+}
+
 test "part 1" {
     const example = test_input;
 
@@ -114,5 +139,5 @@ test "part 2" {
     const example = test_input;
 
     const result = try part2(std.testing.allocator, example);
-    try std.testing.expectEqual(@as(i64, 0), result);
+    try std.testing.expectEqual(@as(i64, 14), result);
 }
