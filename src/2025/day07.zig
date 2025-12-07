07b6103ca8d529b4be853b98f154e73de57a02d5
TsunamiNoAi <spam@falseblue.com>
2025-12-07 15:11:28 -0500

    Start day 7

---
+++ src/2025/day07.zig
@@ -0,0 +1,70 @@
+const std = @import("std");
+const util = @import("util");
+const mvzr = @import("mvzr");
+const Array = std.ArrayList;
+const Allocator = std.mem.Allocator;
+const tst = std.testing;
+const math = std.math;
+
+// Automatically embedded at compile time
+pub const data = @embedFile("data/day07.txt");
+pub const DayNumber = 7;
+
+pub fn part1(allocator: std.mem.Allocator, input: []const u8) !i64 {
+    _ = allocator; // autofix
+    const result: i64 = 0;
+
+    var lines = std.mem.tokenizeScalar(u8, input, '\n');
+    while (lines.next()) |line| {
+        _ = line; // autofix
+        // Your solution here
+    }
+
+    return result;
+}
+
+pub fn part2(allocator: std.mem.Allocator, input: []const u8) !i64 {
+    _ = allocator; // autofix
+    const result: i64 = 0;
+
+    var lines = std.mem.tokenizeScalar(u8, input, '\n');
+    while (lines.next()) |line| {
+        _ = line; // autofix
+        // Your solution here
+    }
+
+    return result;
+}
+
+const test_input =
+    \\.......S.......
+    \\...............
+    \\.......^.......
+    \\...............
+    \\......^.^......
+    \\...............
+    \\.....^.^.^.....
+    \\...............
+    \\....^.^...^....
+    \\...............
+    \\...^.^...^.^...
+    \\...............
+    \\..^...^.....^..
+    \\...............
+    \\.^.^.^.^.^...^.
+    \\...............
+;
+
+test "part 1" {
+    const example = test_input;
+
+    const result = try part1(std.testing.allocator, example);
+    try std.testing.expectEqual(@as(i64, 21), result);
+}
+
+test "part 2" {
+    const example = test_input;
+
+    const result = try part2(std.testing.allocator, example);
+    try std.testing.expectEqual(@as(i64, 0), result);
+}
