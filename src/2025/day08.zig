fb37f1260e5b24bebccf142657ddb5cf3094398b
Tsunami Noai <spam@falseblue.com>
2025-12-08 15:10:43 -0500

    Add initial implementation for Day 8, including Playground structure and box management

--- src/2025/day08.zig
+++ src/2025/day08.zig
@@ -11,13 +11,20 @@ pub const data = @embedFile("data/day08.txt");
 pub const DayNumber = 8;
 
 pub fn part1(allocator: std.mem.Allocator, input: []const u8) !i64 {
-    _ = allocator; // autofix
     const result: i64 = 0;
+
+    var p = Playground.init(allocator);
+    defer p.deinit();
 
     var lines = std.mem.tokenizeScalar(u8, input, '\n');
     while (lines.next()) |line| {
-        _ = line; // autofix
-        // Your solution here
+        var iter = std.mem.splitScalar(u8, line, ',');
+
+        try p.addBox(
+            try std.fmt.parseInt(i64, iter.next() orelse return error.InvalidInput, 10),
+            try std.fmt.parseInt(i64, iter.next() orelse return error.InvalidInput, 10),
+            try std.fmt.parseInt(i64, iter.next() orelse return error.InvalidInput, 10),
+        );
     }
 
     return result;
@@ -35,6 +42,108 @@ pub fn part2(allocator: std.mem.Allocator, input: []const u8) !i64 {
 
     return result;
 }
+
+const CurcuitId = usize;
+const BoxId = usize;
+const Position = struct {
+    x: i64 = 0,
+    y: i64 = 0,
+    z: i64 = 0,
+    pub fn distance(self: Position, other: Position) i64 {
+        const xx = self.x * other.x;
+        const yy = self.y * other.y;
+        const zz = self.z * other.z;
+
+        return std.math.sqrt(@as(usize, @intCast(xx + yy + zz)));
+    }
+};
+const Connection = struct {
+    from: BoxId,
+    to: BoxId,
+    len: i64 = 0,
+};
+const JunctionBox = struct {
+    pos: Position = .{},
+    id: BoxId = 0,
+    connections: Array(BoxId) = .{},
+
+    pub fn init(x: anytype, y: anytype, z: anytype, id: CurcuitId) JunctionBox {
+        return .{
+            .pos = .{
+                .x = @intCast(x),
+                .y = @intCast(y),
+                .z = @intCast(z),
+            },
+            .id = id,
+        };
+    }
+    pub fn distance(self: JunctionBox, other: JunctionBox) i64 {
+        return self.pos.distance(other.pos);
+    }
+};
+
+/// 3d environment
+/// find closest that arent already directly connected
+/// look for shortest loops
+/// return the sum of the largest 3
+const Playground = struct {
+    jboxes: std.AutoArrayHashMap(usize, JunctionBox),
+    maxId: usize = 0,
+    circuits: Array(Array(BoxId)),
+    alloc: Allocator,
+
+    pub fn init(alloc: Allocator) Playground {
+        return .{
+            .alloc = alloc,
+            .jboxes = .init(alloc),
+            .circuits = .{},
+        };
+    }
+    pub fn deinit(self: *Playground) void {
+        for (self.circuits.items) |*c| {
+            c.deinit(self.alloc);
+        }
+        self.circuits.deinit(self.alloc);
+        self.jboxes.deinit();
+    }
+
+    pub fn addBox(self: *Playground, x: i64, y: i64, z: i64) !void {
+        const box = JunctionBox.init(x, y, z, self.maxId);
+
+        try self.jboxes.put(self.maxId, box);
+        self.maxId += 1;
+    }
+
+    fn findClosest(self: Playground, box: anytype) struct { ?BoxId, i64 } {
+        var minDistance: i64 = math.maxInt(i64);
+        var ret: ?BoxId = null;
+        var iter = self.jboxes.iterator();
+        while (iter.next()) |jb| {
+            if (jb.value_ptr.id == box.value_ptr.id) continue; // ignore self
+            const d = box.value_ptr.distance(jb.value_ptr.*);
+            if (d < minDistance) {
+                minDistance = d;
+                ret = jb.value_ptr.id;
+            }
+        }
+        return .{ ret, minDistance };
+    }
+
+    /// By connecting these two junction boxes together, because electricity can flow between them,
+    /// they become part of the same circuit. After connecting them, there is a single circuit which
+    /// contains two junction boxes, and the remaining 18 junction boxes remain in their own individual
+    ///  circuits.
+    /// Now, the two junction boxes which are closest together but aren't already directly connected
+    ///  are 162,817,812 and 431,825,988. After connecting them, since 162,817,812 is already connected
+    ///  to another junction box, there is now a single circuit which contains three junction boxes and
+    /// an additional 17 circuits which contain one junction box each.
+    pub fn connect(self: *Playground) !void {
+        var iter = self.jboxes.iterator();
+        while (iter.next()) |ent| {
+            std.debug.print("{any}\n", .{ent.value_ptr.*});
+        }
+    }
+};
 
 const test_input =
     \\162,817,812
@@ -58,6 +167,26 @@ const test_input =
     \\984,92,344
     \\425,690,689
 ;
+
+test "playground" {
+    var p = Playground.init(tst.allocator);
+    defer p.deinit();
+
+    var lines = std.mem.tokenizeScalar(u8, test_input, '\n');
+    while (lines.next()) |line| {
+        var iter = std.mem.splitScalar(u8, line, ',');
+
+        try p.addBox(
+            try std.fmt.parseInt(i64, iter.next() orelse return error.InvalidInput, 10),
+            try std.fmt.parseInt(i64, iter.next() orelse return error.InvalidInput, 10),
+            try std.fmt.parseInt(i64, iter.next() orelse return error.InvalidInput, 10),
+        );
+    }
+
+    try tst.expectEqual(20, p.jboxes.values().len);
+
+    try p.connect();
+}
 
 test "part 1" {
     const example = test_input;
