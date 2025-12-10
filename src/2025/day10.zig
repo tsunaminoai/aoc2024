06f5b828762d6d139e3d2460251454529c15425a
Tsunami Noai <spam@falseblue.com>
2025-12-10 16:26:29 -0500

    Start day10 with failing test

---
+++ src/2025/day10.zig
@@ -0,0 +1,57 @@
+const std = @import("std");
+const util = @import("util");
+const mvzr = @import("mvzr");
+const Array = std.ArrayList;
+const Allocator = std.mem.Allocator;
+const tst = std.testing;
+const math = std.math;
+
+// Automatically embedded at compile time
+pub const data = @embedFile("data/day01.txt");
+pub const DayNumber = 1;
+
+pub fn part1(allocator: std.mem.Allocator, input: []const u8) !i64 {
+    _ = allocator; // autofix
+    const result: i64 = 0;
+
+    var lines = std.mem.tokenizeScalar(u8, input, '\n');
+    while (lines.next()) |line| {
+        _ = line; // autofix
+        // Your solution here
+    }
+
+    return result;
+}
+
+pub fn part2(allocator: std.mem.Allocator, input: []const u8) !i64 {
+    _ = allocator; // autofix
+    const result: i64 = 0;
+
+    var lines = std.mem.tokenizeScalar(u8, input, '\n');
+    while (lines.next()) |line| {
+        _ = line; // autofix
+        // Your solution here
+    }
+
+    return result;
+}
+
+const test_input =
+    \\[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
+    \\[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}
+    \\[.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}
+;
+
+test "part 1" {
+    const example = test_input;
+
+    const result = try part1(std.testing.allocator, example);
+    try std.testing.expectEqual(@as(i64, 7), result);
+}
+
+test "part 2" {
+    const example = test_input;
+
+    const result = try part2(std.testing.allocator, example);
+    try std.testing.expectEqual(@as(i64, 0), result);
+}
