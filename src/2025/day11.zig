204fed2b103de79568ea9dd072c16038c33b28f4
Tsunami Noai <spam@falseblue.com>
2025-12-11 13:32:43 -0500

    Start day 11 with failing test

---
+++ src/2025/day11.zig
@@ -0,0 +1,84 @@
+const std = @import("std");
+const util = @import("util");
+const mvzr = @import("mvzr");
+const Array = std.ArrayList;
+const Allocator = std.mem.Allocator;
+const tst = std.testing;
+const math = std.math;
+
+// Automatically embedded at compile time
+pub const data = @embedFile("data/day11.txt");
+pub const DayNumber = 11;
+
+pub fn part1(allocator: std.mem.Allocator, input: []const u8) !i64 {
+    _ = allocator; // autofix
+    const result: i64 = 0;
+
+    var lines = std.mem.tokenizeScalar(u8, input, '\n');
+    while (lines.next()) |line| {
+        _ = line; // autofix
+        // Your solution here
+    }
+
+    return result;
+}
+
+pub fn part2(allocator: std.mem.Allocator, input: []const u8) !i64 {
+    _ = allocator; // autofix
+    const result: i64 = 0;
+
+    var lines = std.mem.tokenizeScalar(u8, input, '\n');
+    while (lines.next()) |line| {
+        _ = line; // autofix
+        // Your solution here
+    }
+
+    return result;
+}
+
+pub const Servers = struct {
+    root: ?*Node = null,
+    out: ?*Node = null,
+    nodes: std.StringArrayHashMap(Node),
+    alloc: Allocator,
+
+    pub const Connection = *Node;
+    pub const Node = struct { id: []u8, connections: Array(Connection) };
+
+    pub fn init(alloc: Allocator) Servers {
+        return .{
+            .alloc = alloc,
+            .nodes = .init(alloc),
+        };
+    }
+    pub fn deinit(self: *Servers) void {
+        self.nodes.deinit();
+    }
+};
+
+const test_input =
+    \\aaa: you hhh
+    \\you: bbb ccc
+    \\bbb: ddd eee
+    \\ccc: ddd eee fff
+    \\ddd: ggg
+    \\eee: out
+    \\fff: out
+    \\ggg: out
+    \\hhh: ccc fff iii
+    \\iii: out
+;
+
+test "part 1" {
+    const example = test_input;
+
+    const result = try part1(std.testing.allocator, example);
+    try std.testing.expectEqual(@as(i64, 5), result);
+}
+
+test "part 2" {
+    const example = test_input;
+
+    const result = try part2(std.testing.allocator, example);
+    try std.testing.expectEqual(@as(i64, 0), result);
+}
